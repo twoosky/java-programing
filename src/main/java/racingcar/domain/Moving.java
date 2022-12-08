84c95e51cc742f161a8c87054dc338a8c2095049
twoosky <lhjae3@naver.com>
2022-12-09 04:06:09 +0900

    feat: 랜덤값 생성 및 전진 여부 판단 메서드 구현

---
+++ src/main/java/racingcar/domain/Moving.java
@@ -0,0 +1,25 @@
+package racingcar.domain;
+
+import racingcar.utils.MovingRuleGenerator;
+
+public class Moving {
+    private final MovingRuleGenerator<Integer> movingRuleGenerator;
+
+    public Moving(MovingRuleGenerator<Integer> movingRuleGenerator) {
+        this.movingRuleGenerator = movingRuleGenerator;
+    }
+
+    public void move(Cars cars) {
+        for (Car car : cars.getCars()) {
+            car.move(isMove());
+        }
+    }
+
+    private boolean isMove() {
+        return generateRandomNumber() >= 4;
+    }
+
+    private int generateRandomNumber() {
+        return movingRuleGenerator.generate();
+    }
+}
