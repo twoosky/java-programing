f5a9ec99ca4ecfcd8dfd0e3129c1e4ebbf13d923
twoosky <lhjae3@naver.com>
2022-12-09 03:02:28 +0900

    feat: 에러 메시지 객체 생성

---
+++ src/main/java/racingcar/utils/ErrorMessage.java
@@ -0,0 +1,9 @@
+package racingcar.utils;
+
+public class ErrorMessage {
+    public static final String INVALID_SEPARATOR = "[ERROR] 자동차 이름은 쉼표로 구분해야 합니다.";
+    public static final String NOT_MORE_THAN_ONE = "[ERROR] 자동차 개수는 2개 이상이어야 합니다.";
+    public static final String OUT_OF_NAME_SIZE = "[ERROR] 자동차 이름은 5자 이하여야 합니다.";
+    public static final String DUPLICATED_NAME = "[ERROR] 자동차 이름은 중복이 불가능합니다.";
+    public static final String OUT_OF_RANGE = "[ERROR] 자동차 개수는 2,147,483,647개까지 입력 가능합니다.";
+}
