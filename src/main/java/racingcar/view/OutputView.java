e1e601c11282f9456f5cd98e1b3e9fded222a9d5
twoosky <lhjae3@naver.com>
2022-12-11 00:42:44 +0900

    feat: 출력 객체 및 메서드 작성

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,35 @@
+package racingcar.view;
+
+import racingcar.domain.Car;
+import racingcar.dto.MovingResultDto;
+import racingcar.dto.WinnerDto;
+import java.util.Collections;
+
+import static java.text.MessageFormat.*;
+import static racingcar.utils.ViewMessage.*;
+
+public class OutputView {
+
+    public void printExecuteResultMessage() {
+        System.out.println(EXECUTE_RESULT_MESSAGE);
+    }
+
+    public void printMovingResult(MovingResultDto movingResultDto) {
+        for (Car car : movingResultDto.getCars()) {
+            System.out.println(generateMovingResultMessage(car));
+        }
+    }
+
+    private String generateMovingResultMessage(Car car) {
+        return format(MOVING_RESULT_FORMAT, car.getName(),
+                String.join("", Collections.nCopies(car.getPosition(), MOVING_POSITION_FORMAT)));
+    }
+
+    public void printWinner(WinnerDto winnerDto) {
+        System.out.println(generateWinnerMessage(winnerDto));
+    }
+
+    private String generateWinnerMessage(WinnerDto winnerDto) {
+        return format(WINNER_RESULT_FORMAT, String.join(", ", winnerDto.getNames()));
+    }
+}
