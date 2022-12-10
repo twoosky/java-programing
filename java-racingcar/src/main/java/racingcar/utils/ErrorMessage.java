fd7d7d17299095b1b2bec849b352f44190632786
twoosky <lhjae3@naver.com>
2022-12-11 05:10:50 +0900

    fix: 불필요한 에러메시지 정리

--- java-racingcar/src/main/java/racingcar/utils/ErrorMessage.java
+++ java-racingcar/src/main/java/racingcar/utils/ErrorMessage.java
@@ -2,7 +2,7 @@ package racingcar.utils;
 
 public class ErrorMessage {
     public static final String INVALID_SEPARATOR = "[ERROR] 자동차 이름은 쉼표로 구분해야 합니다.";
-    public static final String LESS_THAN_TWO_CAR_QUANTITY = "[ERROR] 자동차 개수는 2개 이상이어야 합니다.";
+    public static final String LESS_THAN_TWO_CAR_COUNT = "[ERROR] 자동차 개수는 2개 이상이어야 합니다.";
     public static final String OUT_OF_RANGE_NAME_SIZE = "[ERROR] 자동차 이름은 5자 이하여야 합니다.";
     public static final String DUPLICATED_NAME_CAR_NAME = "[ERROR] 자동차 이름은 중복이 불가능합니다.";
 
