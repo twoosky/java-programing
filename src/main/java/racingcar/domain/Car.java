d49f6984318d9b1d6d58e7f05101816652b7424c
twoosky <lhjae3@naver.com>
2022-12-10 22:31:45 +0900

    feat: 현재 위치 반환 메서드 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,5 +1,7 @@
 package racingcar.domain;
 
+import java.util.Comparator;
+
 public class Car {
     private final String name;
     private int position = 0;
@@ -12,5 +14,13 @@ public class Car {
         if (isMove) {
             position += 1;
         }
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public boolean isWinner(int maxPosition) {
+        return position == maxPosition;
     }
 }
