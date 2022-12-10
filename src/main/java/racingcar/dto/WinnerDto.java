2071ca381c824210aa1e7bbb64b5f9b14d712c5e
twoosky <lhjae3@naver.com>
2022-12-10 22:55:50 +0900

    feat: 우승자 이름 dto 생성

---
+++ src/main/java/racingcar/dto/WinnerDto.java
@@ -0,0 +1,13 @@
+package racingcar.dto;
+
+import racingcar.domain.Winner;
+
+import java.util.List;
+
+public class WinnerDto {
+    private final List<String> names;
+
+    public WinnerDto(Winner winner) {
+        this.names = winner.getNames();
+    }
+}
