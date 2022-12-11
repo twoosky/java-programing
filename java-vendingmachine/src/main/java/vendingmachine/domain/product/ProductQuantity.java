47cd0ff7df5384cb8c9a6263faed7fc851c5a094
twoosky <lhjae3@naver.com>
2022-12-12 03:45:39 +0900

    feat: 상품 솔드아웃 여부 확인 메서드 구현

--- java-vendingmachine/src/main/java/vendingmachine/domain/product/ProductQuantity.java
+++ java-vendingmachine/src/main/java/vendingmachine/domain/product/ProductQuantity.java
@@ -1,11 +1,14 @@
 package vendingmachine.domain.product;
 
-import static vendingmachine.utils.ErrorMessages.OUT_OF_RANGE_PRODUCT_QUANTITY;
+import vendingmachine.utils.IntegerConvertor;
+
+import static vendingmachine.utils.ErrorMessages.*;
 
 public class ProductQuantity {
-    private final int quantity;
+    private int quantity;
 
-    public ProductQuantity(int quantity) {
+    public ProductQuantity(String input) {
+        int quantity = IntegerConvertor.parseInt(input, NOT_INTEGER_PRODUCT_QUANTITY);
         validate(quantity);
         this.quantity = quantity;
     }
@@ -18,5 +21,13 @@ public class ProductQuantity {
         if (quantity < 1) {
             throw new IllegalArgumentException(OUT_OF_RANGE_PRODUCT_QUANTITY);
         }
+    }
+
+    public boolean isOutOfStock() {
+        return quantity == 0;
+    }
+
+    public void decrease() {
+        quantity--;
     }
 }
