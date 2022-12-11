fb78b237d0ab191517587bcdf219e6e70ff94dd3
twoosky <lhjae3@naver.com>
2022-12-11 17:22:13 +0900

    feat: 입력값 객체 생성 및 메서드 구현

---
+++ java-vendingmachine/src/main/java/vendingmachine/view/InputView.java
@@ -0,0 +1,35 @@
+package vendingmachine.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import vendingmachine.utils.IntegerConvertor;
+
+import static vendingmachine.utils.ErrorMessages.NOT_INTEGER_CONSUMER_MONEY;
+import static vendingmachine.utils.ErrorMessages.NOT_INTEGER_MACHINE_MONEY;
+
+public class InputView {
+    public int readMachineMoney() {
+        System.out.println("자판기가 보유하고 있는 금액을 입력해 주세요.");
+        return IntegerConvertor.parseInt(
+                Console.readLine(),
+                NOT_INTEGER_MACHINE_MONEY
+        );
+    }
+
+    public String readProduct() {
+        System.out.println("상품명과 가격, 수량을 입력해 주세요.");
+        return Console.readLine();
+    }
+
+    public int readConsumerMoney() {
+        System.out.println("투입 금액을 입력해 주세요.");
+        return IntegerConvertor.parseInt(
+                Console.readLine(),
+                NOT_INTEGER_CONSUMER_MONEY
+        );
+    }
+
+    public String readBuyingProduct() {
+        System.out.println("구매할 상품명을 입력해 주세요.");
+        return Console.readLine();
+    }
+}
