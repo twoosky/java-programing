0a81d69ab9583eb265c6b317cd09df6d42799f27
twoosky <lhjae3@naver.com>
2022-12-13 01:27:04 +0900

    feat(Input): 미션 입력값 검증 로직 추가

--- java-pairmatching-precourse/src/main/java/pairmatching/view/InputView.java
+++ java-pairmatching-precourse/src/main/java/pairmatching/view/InputView.java
@@ -1,18 +1,21 @@
 package pairmatching.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import pairmatching.view.validator.InputMissionValidator;
 
 import static pairmatching.utils.ViewMessages.*;
 
 public class InputView {
     public String readMenu() {
-        System.out.println(MENU);
+        System.out.println(MISSION);
         return Console.readLine();
     }
 
-    public String readCourse() {
+    public String readMission() {
         System.out.println(COURSE);
-        return Console.readLine();
+        String mission = Console.readLine();
+        InputMissionValidator.validate(mission);
+        return mission;
     }
 
     public String readReMatching() {
