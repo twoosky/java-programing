aa3c38d9cac9182aec28159efa089312ca95e99d
twoosky <lhjae3@naver.com>
2022-12-13 03:59:51 +0900

    refactor(Mission): 해당 미션 페어 존재 여부 반환 메서드 이동

--- java-pairmatching-precourse/src/main/java/pairmatching/domain/Mission.java
+++ java-pairmatching-precourse/src/main/java/pairmatching/domain/Mission.java
@@ -1,20 +1,18 @@
 package pairmatching.domain;
 
 public enum Mission {
-    CAR("자동차경주", false),
-    LOTTO("로또", false),
-    BASEBALL("숫자야구게임", false),
-    BUCKET("장바구니", false),
-    PAY("결제", false),
-    SUBWAY("지하철노선도", false),
-    PERFORMANCE("성능개선", false),
-    DEPLOY("배포", false);
+    CAR("자동차경주"),
+    LOTTO("로또"),
+    BASEBALL("숫자야구게임"),
+    BUCKET("장바구니"),
+    PAY("결제"),
+    SUBWAY("지하철노선도"),
+    PERFORMANCE("성능개선"),
+    DEPLOY("배포");
 
     private final String name;
-    private final boolean history;
 
-    Mission(String name, boolean history) {
+    Mission(String name) {
         this.name = name;
-        this.history = history;
     }
 }
