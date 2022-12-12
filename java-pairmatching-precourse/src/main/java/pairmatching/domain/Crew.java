6c107dcaa6db4ed46476a09b8fb5a432fe56d17d
twoosky <lhjae3@naver.com>
2022-12-13 04:00:22 +0900

    feat(Crew): equals 재정의

--- java-pairmatching-precourse/src/main/java/pairmatching/domain/Crew.java
+++ java-pairmatching-precourse/src/main/java/pairmatching/domain/Crew.java
@@ -1,5 +1,7 @@
 package pairmatching.domain;
 
+import java.util.Objects;
+
 public class Crew {
     private Course course;
     private String name;
@@ -7,5 +9,26 @@ public class Crew {
     public Crew(Course course, String name) {
         this.course = course;
         this.name = name;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Crew crew = (Crew) o;
+        return course == crew.course && Objects.equals(name, crew.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(course, name);
     }
 }
