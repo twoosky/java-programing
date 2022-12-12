d2165f2e0a6217a36ab1c8fa6976d0bc7e8e0e99
twoosky <lhjae3@naver.com>
2022-12-13 01:59:20 +0900

    refactor(Level): mission 변수 추가

--- java-pairmatching-precourse/src/main/java/pairmatching/domain/Level.java
+++ java-pairmatching-precourse/src/main/java/pairmatching/domain/Level.java
@@ -1,25 +1,29 @@
 package pairmatching.domain;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 import static pairmatching.utils.ErrorMessages.NOT_EXIST_LEVEL;
 
 public enum Level {
-    LEVEL1("레벨1"),
-    LEVEL2("레벨2"),
-    LEVEL3("레벨3"),
-    LEVEL4("레벨4"),
-    LEVEL5("레벨5");
+    LEVEL1("레벨1", Arrays.asList(Mission.CAR, Mission.LOTTO, Mission.BASEBALL)),
+    LEVEL2("레벨2", Arrays.asList(Mission.BUCKET, Mission.PAY, Mission.SUBWAY)),
+    LEVEL3("레벨3", Collections.emptyList()),
+    LEVEL4("레벨4", Arrays.asList(Mission.PERFORMANCE, Mission.DEPLOY)),
+    LEVEL5("레벨5", Collections.emptyList());
 
-    private String name;
+    private final String name;
+    private final List<Mission> missions;
 
-    Level(String name) {
+    Level(String name, List<Mission> missions) {
         this.name = name;
+        this.missions = missions;
     }
 
     public static Level of(String name) {
         return Arrays.stream(Level.values())
-                .filter(level -> level.name == name)
+                .filter(level -> level.name.equals(name))
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException(NOT_EXIST_LEVEL));
     }
