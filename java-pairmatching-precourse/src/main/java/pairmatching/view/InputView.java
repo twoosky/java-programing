3b2829f2247c8875ad2ecada6ac7ee4f12aa0d74
twoosky <lhjae3@naver.com>
2022-12-13 01:36:08 +0900

    fix(Input): 미션 정보 입력값 MissionDto로 변경해 반환하도록 수정

--- java-pairmatching-precourse/src/main/java/pairmatching/view/InputView.java
+++ java-pairmatching-precourse/src/main/java/pairmatching/view/InputView.java
@@ -1,25 +1,43 @@
 package pairmatching.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import pairmatching.dto.MissionDto;
 import pairmatching.view.validator.InputMissionValidator;
 
 import static pairmatching.utils.ViewMessages.*;
 
 public class InputView {
-    public String readMenu() {
+    public static final String MISSION_INFO_SEPARATOR = ", ";
+    public static final int COURSE_INDEX = 0;
+    public static final int LEVEL_INDEX = 1;
+    public static final int MISSION_INDEX = 2;
+
+
+    public String readFunction() {
         System.out.println(MISSION);
         return Console.readLine();
     }
 
-    public String readMission() {
+    public MissionDto readMission() {
         System.out.println(COURSE);
         String mission = Console.readLine();
         InputMissionValidator.validate(mission);
-        return mission;
+        return generateMissionDto(mission);
     }
 
     public String readReMatching() {
         System.out.println(REMATCHING);
         return Console.readLine();
     }
+
+    private MissionDto generateMissionDto(String mission) {
+        String[] missionInfo = mission.split(MISSION_INFO_SEPARATOR);
+        return new MissionDto(
+                missionInfo[COURSE_INDEX],
+                missionInfo[LEVEL_INDEX],
+                missionInfo[MISSION_INDEX]
+        );
+    }
+
+
 }
