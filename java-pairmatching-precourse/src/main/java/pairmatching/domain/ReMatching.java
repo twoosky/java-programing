ea41aa6d3cd4dbc687b6fe004afedc797f389914
twoosky <lhjae3@naver.com>
2022-12-13 02:04:40 +0900

    feat(ReMatching): 재매칭 여부값 갖는 객체 생성 및 입력값 검증 메서드 구현

---
+++ java-pairmatching-precourse/src/main/java/pairmatching/domain/ReMatching.java
@@ -0,0 +1,24 @@
+package pairmatching.domain;
+
+import static pairmatching.utils.ErrorMessages.INVALID_RE_MATCHING_FLAG;
+
+public class ReMatching {
+    public static final String REMATCHING = "네";
+    public static final String NOT_REMATCHING = "아니오";
+
+    private final String reMatching;
+
+    public ReMatching(String reMatching) {
+        this.reMatching = reMatching;
+    }
+
+    private void validate(String input) {
+        if(!isValidReMatchingFlag(input)) {
+            throw new IllegalArgumentException(INVALID_RE_MATCHING_FLAG);
+        }
+    }
+
+    private boolean isValidReMatchingFlag(String input) {
+        return input.equals(REMATCHING) || input.equals(NOT_REMATCHING);
+    }
+}
