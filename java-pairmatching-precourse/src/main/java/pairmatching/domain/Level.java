02cfd72ecc587d54af9462b9df12e12b2d4608b0
twoosky <lhjae3@naver.com>
2022-12-13 01:19:06 +0900

    feat(Level): 정적 팩터리 메서드 구현

--- java-pairmatching-precourse/src/main/java/pairmatching/domain/Level.java
+++ java-pairmatching-precourse/src/main/java/pairmatching/domain/Level.java
@@ -1,5 +1,9 @@
 package pairmatching.domain;
 
+import java.util.Arrays;
+
+import static pairmatching.utils.ErrorMessages.NOT_EXIST_LEVEL;
+
 public enum Level {
     LEVEL1("레벨1"),
     LEVEL2("레벨2"),
@@ -11,5 +15,12 @@ public enum Level {
 
     Level(String name) {
         this.name = name;
+    }
+
+    public static Level of(String name) {
+        return Arrays.stream(Level.values())
+                .filter(level -> level.name == name)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(NOT_EXIST_LEVEL));
     }
 }
