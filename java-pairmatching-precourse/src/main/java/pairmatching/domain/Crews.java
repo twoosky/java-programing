899cb98ab476335ccba4323493dbbcc6d4b9bece
twoosky <lhjae3@naver.com>
2022-12-13 02:28:15 +0900

    feat(Crews): 매칭된 크루 반환 메서드 구현

--- java-pairmatching-precourse/src/main/java/pairmatching/domain/Crews.java
+++ java-pairmatching-precourse/src/main/java/pairmatching/domain/Crews.java
@@ -1,6 +1,7 @@
 package pairmatching.domain;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static pairmatching.utils.FileReader.*;
@@ -20,5 +21,20 @@ public class Crews {
 
     private void addCrew(Course course, List<String> names) {
         names.forEach(name -> crews.add(new Crew(course, name)));
+    }
+
+    public List<Crew> getCrews() {
+        return crews;
+    }
+
+    public List<Crew> matchPair(int index) {
+        if (isLastPair(index)) {
+            return Arrays.asList(crews.get(index++), crews.get(index++), crews.get(index));
+        }
+        return Arrays.asList(crews.get(index++), crews.get(index));
+    }
+
+    private boolean isLastPair(int index) {
+        return ((crews.size() - 1) - index) == 3;
     }
 }
