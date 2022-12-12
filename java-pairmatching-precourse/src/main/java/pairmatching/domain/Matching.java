39210d72f11a869746651145d11fd3fe10fe9fdb
twoosky <lhjae3@naver.com>
2022-12-13 03:59:14 +0900

    refactor(Matching): 페어 생성 메서드 객체 이동

---
+++ java-pairmatching-precourse/src/main/java/pairmatching/domain/Matching.java
@@ -0,0 +1,43 @@
+package pairmatching.domain;
+
+import pairmatching.utils.PairMatchingRule;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Matching {
+    private final PairMatchingRule<Crew> pairMatchingRule;
+
+    public Matching(PairMatchingRule<Crew> pairMatchingRule) {
+        this.pairMatchingRule = pairMatchingRule;
+    }
+
+    public Pair match(Crews crews) {
+        List<Crew> randomCrews = shufflesCrews(crews);
+        return new Pair(matchPair(randomCrews));
+    }
+
+    private List<Crew> shufflesCrews(Crews crews) {
+        return pairMatchingRule.match(crews.getCrews());
+    }
+
+    private List<List<Crew>> matchPair(List<Crew> crews) {
+        return IntStream.range(0, crews.size())
+                .filter(i -> i % 2 == 0)
+                .mapToObj(index -> generatePair(index, crews))
+                .collect(Collectors.toList());
+    }
+
+    private List<Crew> generatePair(int index, List<Crew> crews) {
+        if (isLastPair(index, crews)) {
+            return Arrays.asList(crews.get(index++), crews.get(index++), crews.get(index));
+        }
+        return Arrays.asList(crews.get(index++), crews.get(index));
+    }
+
+    private boolean isLastPair(int index, List<Crew> crews) {
+        return ((crews.size() - 1) - index) == 3;
+    }
+}
