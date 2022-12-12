f38f0872b7a2561381112d41bb7cd664af8867a3
twoosky <lhjae3@naver.com>
2022-12-13 04:01:11 +0900

    feat(Crews): 인스턴스 변수에 crewPairHistory 추가 및 매칭 이력 여부 반환 메서드 구현

--- java-pairmatching-precourse/src/main/java/pairmatching/domain/Crews.java
+++ java-pairmatching-precourse/src/main/java/pairmatching/domain/Crews.java
@@ -3,14 +3,18 @@ package pairmatching.domain;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import static pairmatching.utils.ErrorMessages.NOT_EXIST_CREW;
 import static pairmatching.utils.FileReader.*;
 
 public class Crews {
     private final List<Crew> crews;
+    private final List<CrewPairHistory> crewPairHistories;
 
     public Crews() {
         this.crews = new ArrayList<>();
+        this.crewPairHistories = initCrewPairHistories();
         initCrews();
     }
 
@@ -19,22 +23,29 @@ public class Crews {
         addCrew(Course.BACKEND, read(BACKEND_CREWS_FILE_PATH));
     }
 
+    private List<CrewPairHistory> initCrewPairHistories() {
+        return crews.stream()
+                .map(CrewPairHistory::new)
+                .collect(Collectors.toList());
+    }
+
     private void addCrew(Course course, List<String> names) {
         names.forEach(name -> crews.add(new Crew(course, name)));
     }
 
+    public boolean isMatchedPair(Crew crew) {
+        CrewPairHistory crewPairHistory = getCrewPairHistory(crew);
+        return crewPairHistory.isMatchedPair(crew);
+    }
+
+    private CrewPairHistory getCrewPairHistory(Crew crew) {
+        return crewPairHistories.stream()
+                .filter(history-> history.getCrew().equals(crew))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(NOT_EXIST_CREW));
+    }
+
     public List<Crew> getCrews() {
         return crews;
-    }
-
-    public List<Crew> matchPair(int index) {
-        if (isLastPair(index)) {
-            return Arrays.asList(crews.get(index++), crews.get(index++), crews.get(index));
-        }
-        return Arrays.asList(crews.get(index++), crews.get(index));
-    }
-
-    private boolean isLastPair(int index) {
-        return ((crews.size() - 1) - index) == 3;
     }
 }
