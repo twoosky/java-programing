78295e684d434c2a35b6c674d17a42ffcf593998
twoosky <lhjae3@naver.com>
2022-12-13 01:36:33 +0900

    feat(MissionDto): 미션 정보 담는dto 생성

---
+++ java-pairmatching-precourse/src/main/java/pairmatching/dto/MissionDto.java
@@ -0,0 +1,13 @@
+package pairmatching.dto;
+
+public class MissionDto {
+    private final String course;
+    private final String level;
+    private final String mission;
+
+    public MissionDto(String course, String level, String mission) {
+        this.course = course;
+        this.level = level;
+        this.mission = mission;
+    }
+}
---
+++ java-pairmatching-precourse/src/main/java/pairmatching/dto/MissionMenuDto.java
@@ -0,0 +1,4 @@
+package pairmatching.dto;
+
+public class MissionMenuDto {
+}
