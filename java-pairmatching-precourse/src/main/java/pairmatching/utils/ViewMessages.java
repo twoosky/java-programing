2634e61ee4f132cd137175594af32f921aea9b5c
twoosky <lhjae3@naver.com>
2022-12-13 00:51:35 +0900

    feat(ViewMessages): 입력 메시지 상수 선언

---
+++ java-pairmatching-precourse/src/main/java/pairmatching/utils/ViewMessages.java
@@ -0,0 +1,7 @@
+package pairmatching.utils;
+
+public class ViewMessages {
+    public static final String MENU = "기능을 선택하세요.\n1. 페어 매칭\n2. 페어 조회\n3. 페어 초기화\nQ. 종료";
+    public static final String COURSE = "과정, 레벨, 미션을 선택하세요.";
+    public static final String REMATCHING = "매칭 정보가 있습니다. 다시 매칭하시겠습니까?\n네 | 아니오";
+}
